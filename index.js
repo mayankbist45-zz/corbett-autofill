ba9e5c928fb18231dd6496c4590c8d993d391220
Mayank Bist <mayankbist45@gmail.com>
2021-03-13 19:47:20 +0530

    intial release

--- index.js
+++ index.js
@@ -1,41 +1,66 @@
 const express = require("express");
 const app = express();
-const startupDebugger = require("debug")("app:startup");
-const dbDebugger = require("debug")("app:db");
-const morgan = require("morgan");
-const config = require("config");
 const cors = require("cors");
+require("./prod");
 
 // database
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/test", {
+const uri = `mongodb+srv://corbett:zpTiC0denyk3iesn@cluster0.ll84g.mongodb.net/test`;
+// const uri = `mongodb://localhost/playground`;
+mongoose.connect(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-
 const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error"));
+db.on("error", console.error.bind(console, "Connection error"));
 db.once("open", function () {
-  dbDebugger("We are connected");
+  console.log("We are connected");
+});
+const Schema = mongoose.Schema({
+  name: String,
+  age: Number,
+  gender: String,
+  phone: Number,
+  country: {
+    type: String,
+    default: "India",
+  },
+  documentType: Number,
+  documentNumber: Number,
 });
 
+const User = mongoose.model("userModel", Schema);
+
+async function getAllUsers() {
+  const users = await User.find();
+  console.log(users);
+  return users;
+}
+
+async function addData(data) {
+  const user = new User(data);
+  return await user.save();
+}
+
+// express
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(cors());
 
-if (app.get("env") === "development") {
-  app.use(morgan("tiny"));
-  startupDebugger("morgan is enabled");
-}
-
-app.post("/users", (req, res) => {
-  console.log(req.body);
+app.post("/users", async (req, res) => {
+  await addData(req.body);
+  res.send(await getAllUsers());
 });
 
-app.get("/users", (req, res) => {
-  res.send("working properly");
+app.get("/users", async (req, res) => {
+  res.send(await getAllUsers());
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => startupDebugger(`listening on port ${PORT}`));
+app.delete("/users/:id", async (req, res) => {
+  await User.findByIdAndRemove(req.params.id);
+  res.send(await getAllUsers());
+});
+
+const PORT = process.env.PORT || 8021;
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
